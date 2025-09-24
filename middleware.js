46ace647ab7f5850007b890c942f0b161e6822f7
kalyani-09 <kalyanimogre2003@mail.com>
2025-09-24 20:04:49 +0530

    changes in middleware for minimum size

--- middleware.js
+++ middleware.js
@@ -2,7 +2,6 @@
 
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-import { arcjetMiddleware } from "./lib/arcjetMiddleware";
 
 // define which routes you want protected
 const isProtectedRoute = createRouteMatcher([
@@ -12,14 +11,7 @@ const isProtectedRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
-  // 1. Run Arcjet first
-  const ajResponse = await arcjetMiddleware(req);
-  if (ajResponse) {
-    // If Arcjet decided to block or respond, return that
-    return ajResponse;
-  }
-
-  // 2. Then your Clerk logic
+  // Clerk logic
   const { userId, redirectToSignIn, isAuthenticated } = await auth();
 
   // If route is a protected route and user not authenticated → redirect
